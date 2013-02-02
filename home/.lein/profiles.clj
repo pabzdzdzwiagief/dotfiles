b2ee5ff82c21907a65dfa82c89cb87fa29ea13cb
pabzdzdzwiagief <pabzdzdzwiagief@gmail.com>
2013-02-02 18:54:56 +0100

    Add basic profile file for leiningen2

---
+++ home/.lein/profiles.clj
@@ -0,0 +1,4 @@
+{:user {:dependencies [[org.clojure/clojure "1.4.0"]]
+        :plugins [[lein-marginalia "0.7.1"]
+                  [lein-pprint "1.1.1"]
+                  [org.clojure/tools.nrepl "0.2.1"]]}}
