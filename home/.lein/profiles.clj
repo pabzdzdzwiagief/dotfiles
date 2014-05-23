7ab713d3696df09df65a4130dca583c219e56ff3
pabzdzdzwiagief <pabzdzdzwiagief@gmail.com>
2014-05-23 23:07:41 +0200

    Update leiningen profile

--- home/.lein/profiles.clj
+++ home/.lein/profiles.clj
@@ -1,4 +1,4 @@
-{:user {:dependencies [[org.clojure/clojure "1.4.0"]]
+{:user {:dependencies [[org.clojure/clojure "1.6.0"]]
         :plugins [[lein-marginalia "0.7.1"]
                   [lein-pprint "1.1.1"]
-                  [org.clojure/tools.nrepl "0.2.1"]]}}
+                  [cider/cider-nrepl "0.6.0"]]}}
