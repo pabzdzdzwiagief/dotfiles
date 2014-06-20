2fefd77b5d7d19e5186381e2d8ab1862aff7db36
pabzdzdzwiagief <pabzdzdzwiagief@gmail.com>
2014-06-20 22:28:45 +0200

    Change defun order in `init.el`

--- home/.emacs.d/init.el
+++ home/.emacs.d/init.el
@@ -8,6 +8,9 @@
   (init-elisp)
   (init-rust))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Basic config
+
 (defun init-basic ()
   "Setup some very basic functionality"
   (elpa-set-repos)
@@ -53,6 +56,109 @@
   (add-to-list 'load-path "~/.emacs.d/site-lisp")
   (let ((default-directory  "~/.emacs.d/site-lisp"))
     (normal-top-level-add-subdirs-to-load-path)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; autosave
+;; trick - http://snarfed.org/gnu_emacs_backup_files
+
+(defun autosave-to-home ()
+  "Make emacs put autosave files (ie #foo#) and backup files (ie foo~) in
+   ~/.emacs.d/ and create the autosave dir if necessary, since emacs won't"
+  (custom-set-variables
+   '(auto-save-file-name-transforms '((".*" "~/.emacs.d/autosaves/\\1" t)))
+   '(backup-directory-alist '((".*" . "~/.emacs.d/backups/"))))
+  (make-directory "~/.emacs.d/autosaves/" t)
+  (make-directory "~/.emacs.d/backups/" t))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; utf-8
+;; trick - http://gentoo-wiki.com/HOWTO_Make_your_system_use_unicode/utf-8#Editors
+
+(defun use-utf-8 ()
+  "Make emacs use UTF-8"
+  (setq locale-coding-system 'utf-8-unix)
+  (setq terminal-coding-system 'utf-8-unix)
+  (setq keyboard-coding-system 'utf-8-unix)
+  (setq selection-coding-system 'utf-8-unix)
+  (setq prefer-coding-system 'utf-8-unix))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; general look
+
+(defun unclutter-emacs-window ()
+  "Remove scrollbars, toolbars etc."
+  (custom-set-variables
+   '(color-theme-is-global nil)
+   '(inhibit-startup-screen t)
+   '(tool-bar-mode nil)
+   '(menu-bar-mode nil)
+   '(scroll-bar-mode nil)
+   '(show-paren-mode t)
+   '(column-number-mode t)
+   '(blink-cursor-mode nil))
+  (global-hl-line-mode t))
+
+(defun set-frame-look ()
+  "Set font and theme for emacs frames"
+  (load-theme 'wombat)
+  (let ((font-name (first-available-font '("Source Code Pro Semibold"
+                                           "Inconsolata"
+                                           "DejaVu Sans Mono"
+                                           "Monaco"
+                                           "Consolas"
+                                           "Courier New"))))
+    (when font-name
+      (let* ((font-size 10)
+             (font (format "%s-%i" font-name font-size)))
+        (add-to-list 'default-frame-alist `(font . ,font)))))
+  (add-to-list 'default-frame-alist '(cursor-color . "red"))
+  (set-face-background 'highlight "#300000")
+  (set-face-foreground 'highlight nil)
+  (set-face-underline-p 'highlight nil))
+
+(defun first-available-font (font-name-list)
+  "Returns first available font from the list"
+  (let ((font-available-p (lambda (name)
+                            (if (find-font (font-spec :name name)) name))))
+    (car (delq nil (mapcar font-available-p font-name-list)))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; behaviour
+
+(defun configure-behaviour ()
+  "Configure certain aspects of emacs' behaviour"
+  (setq scroll-step 1)
+  (setq-default tab-width 8)
+  (setq-default ident-tabs-mode nil)
+  (setq-default require-final-newline t)
+  (setq-default truncate-lines t)
+  (add-hook 'before-save-hook 'delete-trailing-whitespace)
+  (add-hook 'after-init-hook 'enable-zlc)
+  (add-hook 'after-init-hook 'reuse-buffer-in-dired)
+  (add-hook 'after-init-hook 'side-with-evil)
+  (configure-key-bindings))
+
+(defun configure-key-bindings ()
+  "Assign additional key bindings"
+  (global-set-key (kbd "C-=") 'er/expand-region))
+
+(defun always-use-horizontal-split ()
+  "Make emacs always split horizontally"
+  (setq split-height-threshold nil)
+  (setq split-width-threshold 0))
+
+(defun enable-zlc ()
+  "Enable zsh-like menu completion"
+  (zlc-mode t))
+
+(defun reuse-buffer-in-dired ()
+  "Reuse current `dired' buffer when entering new directory with `a' key"
+  (put 'dired-find-alternate-file 'disabled nil))
+
+(defun side-with-evil ()
+  "Introduce some VI-like behaviour to emacs"
+  (setq evil-default-cursor t)
+  (evil-mode t))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; general programming
@@ -172,109 +278,6 @@
 
 (defun init-rust ()
   (expect-packages '(rust-mode)))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; autosave
-;; trick - http://snarfed.org/gnu_emacs_backup_files
-
-(defun autosave-to-home ()
-  "Make emacs put autosave files (ie #foo#) and backup files (ie foo~) in
-   ~/.emacs.d/ and create the autosave dir if necessary, since emacs won't"
-  (custom-set-variables
-   '(auto-save-file-name-transforms '((".*" "~/.emacs.d/autosaves/\\1" t)))
-   '(backup-directory-alist '((".*" . "~/.emacs.d/backups/"))))
-  (make-directory "~/.emacs.d/autosaves/" t)
-  (make-directory "~/.emacs.d/backups/" t))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; utf-8
-;; trick - http://gentoo-wiki.com/HOWTO_Make_your_system_use_unicode/utf-8#Editors
-
-(defun use-utf-8 ()
-  "Make emacs use UTF-8"
-  (setq locale-coding-system 'utf-8-unix)
-  (setq terminal-coding-system 'utf-8-unix)
-  (setq keyboard-coding-system 'utf-8-unix)
-  (setq selection-coding-system 'utf-8-unix)
-  (setq prefer-coding-system 'utf-8-unix))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; look
-
-(defun unclutter-emacs-window ()
-  "Remove scrollbars, toolbars etc."
-  (custom-set-variables
-   '(color-theme-is-global nil)
-   '(inhibit-startup-screen t)
-   '(tool-bar-mode nil)
-   '(menu-bar-mode nil)
-   '(scroll-bar-mode nil)
-   '(show-paren-mode t)
-   '(column-number-mode t)
-   '(blink-cursor-mode nil))
-  (global-hl-line-mode t))
-
-(defun set-frame-look ()
-  "Set font and theme for emacs frames"
-  (load-theme 'wombat)
-  (let ((font-name (first-available-font '("Source Code Pro Semibold"
-                                           "Inconsolata"
-                                           "DejaVu Sans Mono"
-                                           "Monaco"
-                                           "Consolas"
-                                           "Courier New"))))
-    (when font-name
-      (let* ((font-size 10)
-             (font (format "%s-%i" font-name font-size)))
-        (add-to-list 'default-frame-alist `(font . ,font)))))
-  (add-to-list 'default-frame-alist '(cursor-color . "red"))
-  (set-face-background 'highlight "#300000")
-  (set-face-foreground 'highlight nil)
-  (set-face-underline-p 'highlight nil))
-
-(defun first-available-font (font-name-list)
-  "Returns first available font from the list"
-  (let ((font-available-p (lambda (name)
-                            (if (find-font (font-spec :name name)) name))))
-    (car (delq nil (mapcar font-available-p font-name-list)))))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; behaviour
-
-(defun configure-behaviour ()
-  "Configure certain aspects of emacs' behaviour"
-  (setq scroll-step 1)
-  (setq-default tab-width 8)
-  (setq-default ident-tabs-mode nil)
-  (setq-default require-final-newline t)
-  (setq-default truncate-lines t)
-  (add-hook 'before-save-hook 'delete-trailing-whitespace)
-  (add-hook 'after-init-hook 'enable-zlc)
-  (add-hook 'after-init-hook 'reuse-buffer-in-dired)
-  (add-hook 'after-init-hook 'side-with-evil)
-  (configure-key-bindings))
-
-(defun configure-key-bindings ()
-  "Assign additional key bindings"
-  (global-set-key (kbd "C-=") 'er/expand-region))
-
-(defun always-use-horizontal-split ()
-  "Make emacs always split horizontally"
-  (setq split-height-threshold nil)
-  (setq split-width-threshold 0))
-
-(defun enable-zlc ()
-  "Enable zsh-like menu completion"
-  (zlc-mode t))
-
-(defun reuse-buffer-in-dired ()
-  "Reuse current `dired' buffer when entering new directory with `a' key"
-  (put 'dired-find-alternate-file 'disabled nil))
-
-(defun side-with-evil ()
-  "Introduce some VI-like behaviour to emacs"
-  (setq evil-default-cursor t)
-  (evil-mode t))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Go!
