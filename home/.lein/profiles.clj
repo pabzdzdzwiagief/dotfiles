277c90d0bb73de39c19cc39cccc8f462305f90ba
pabzdzdzwiagief <pabzdzdzwiagief@gmail.com>
2014-11-22 19:30:13 +0100

    Update Cider version in leiningen profile

--- home/.lein/profiles.clj
+++ home/.lein/profiles.clj
@@ -1,4 +1,4 @@
 {:user {:dependencies [[org.clojure/clojure "1.6.0"]]
         :plugins [[lein-marginalia "0.7.1"]
                   [lein-pprint "1.1.1"]
-                  [cider/cider-nrepl "0.7.0"]]}}
+                  [cider/cider-nrepl "0.8.1"]]}}
